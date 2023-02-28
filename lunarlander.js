a7f12f6779bda288ea320780b76abc7d0c95fc30
Ida <idajosefinolsson@gmail.com>
2023-02-28 11:47:03 +0100

    organizing

--- lunarlander.js
+++ lunarlander.js
@@ -1,4 +1,4 @@
-let state = "result";
+let state = "start";
 let gameTimer = 0;
 let starX = [];
 let starY = [];
@@ -8,6 +8,7 @@ let y = 0;
 let speed = 1;
 let velocity = 0.2;
 let isGameActive = true;
+let buttonIsClicked = false;
 
 function setup() {
   createCanvas(700, 700);
@@ -36,10 +37,6 @@ function startScreen() {
   }
   }
 
-  function mouseClicked() {
-    console.log("Button was clicked");
-  }
-
   function gameScreen() {
     noStroke();
     background(0, 0, 0);
@@ -48,172 +45,173 @@ function startScreen() {
           ellipse(starX[index], starY[index], 2);
           starAlpha[index] = starAlpha[index] + 0.02;
       }
-  //falcon
+      
+//falcon
 function falcon(x, y) {
   push();
   translate (x, y);
   noStroke();
   fill(215, 213, 215);
-
-//wing right
-beginShape();
-vertex(189, 121);
-vertex(189, 79);
-vertex(193, 79);
-vertex(214, 121);
-endShape(); 
-
-//wing left
-beginShape();
-vertex(171, 121);
-vertex(171, 79);
-vertex(167, 79);
-vertex(145, 121);
-endShape(); 
-
-//body of falcon
-stroke(0, 0, 0);
-strokeWeight(2);
-fill(215, 213, 215);
-ellipse(180, 140, 80, 80);
-
-//right added side to falcon
-beginShape();
-vertex(180, 140);
-vertex(220, 130);
-vertex(220, 150);
-vertex(180, 140);
-endShape();
-
-//left added side to falcon
-beginShape();
-vertex(180, 140);
-vertex(140, 130);
-vertex(140, 150);
-vertex(180, 140);
-endShape();
-
-//added shapes on top of falcon
-rect(172, 87, 16, 50);
-ellipse(180, 140, 20, 20);
-
-//black dots
-fill(0,0,0);
-ellipse(193, 157, 5, 5);
-ellipse(180, 160, 5, 5);
-ellipse(168, 157, 5, 5);
-ellipse(196, 167, 5, 5);
-ellipse(180, 170, 5, 5);
-ellipse(164, 167, 5, 5);
-
-//added weird thing on top
-stroke(0, 0, 0);
-strokeWeight(2);
-fill(215, 213, 215);
-beginShape();
-vertex(197, 124);
-vertex(211, 118);
-vertex(211, 111);
-vertex(219, 111);
-vertex(219, 151);
-vertex(219, 124);
-vertex(201, 129);
-vertex(197, 124);
-endShape();
-pop();
+  
+  //wing right
+  beginShape();
+  vertex(189, 121);
+  vertex(189, 79);
+  vertex(193, 79);
+  vertex(214, 121);
+  endShape(); 
+  
+  //wing left
+  beginShape();
+  vertex(171, 121);
+  vertex(171, 79);
+  vertex(167, 79);
+  vertex(145, 121);
+  endShape();
+  
+  //body of falcon
+  stroke(0, 0, 0);
+  strokeWeight(2);
+  fill(215, 213, 215);
+  ellipse(180, 140, 80, 80);
+  
+  //right added side to falcon
+  beginShape();
+  vertex(180, 140);
+  vertex(220, 130);
+  vertex(220, 150);
+  vertex(180, 140);
+  endShape();
+  
+  //left added side to falcon
+  beginShape();
+  vertex(180, 140);
+  vertex(140, 130);
+  vertex(140, 150);
+  vertex(180, 140);
+  endShape();
+  
+  //added shapes on top of falcon
+  rect(172, 87, 16, 50);
+  ellipse(180, 140, 20, 20);
+  
+  //black dots
+  fill(0,0,0);
+  ellipse(193, 157, 5, 5);
+  ellipse(180, 160, 5, 5);
+  ellipse(168, 157, 5, 5);
+  ellipse(196, 167, 5, 5);
+  ellipse(180, 170, 5, 5);
+  ellipse(164, 167, 5, 5);
+  
+  //added weird thing on top
+  stroke(0, 0, 0);
+  strokeWeight(2);
+  fill(215, 213, 215);
+  beginShape();
+  vertex(197, 124);
+  vertex(211, 118);
+  vertex(211, 111);
+  vertex(219, 111);
+  vertex(219, 151);
+  vertex(219, 124);
+  vertex(201, 129);
+  vertex(197, 124);
+  endShape();
+  pop();
 }
 
 function scenery() {
-//ground
-fill(210,170,109);
-rect(0, 600, 700);
-
-//death star
-push();
-fill(102, 102, 102);
-ellipse(500, 145, 80, 80);
-pop();
-push();
-fill(102, 105, 102);
-stroke(0,0,0);
-strokeWeight(3);
-ellipse(490, 132, 25, 25);
-rect(460, 155, 80, 0.1);
-pop();
-
-//mos eisley buildings
-push();
-translate(0, 80);
-stroke(166, 120, 51);
-strokeWeight(3);
-
-//building 1
-fill(204, 158, 89);
-ellipse(450, 470, 60, 60);
-rect(400, 470, 50, 70);
-rect(450, 470, 50, 50);
-rect(415, 510, 20, 30);
-push();
-translate(450, 425);
-rotate(0.8);
-rect(0, 0, 10, 10);
-pop();
-
-//building 2 (pole)
-rect(500, 400, 3, 120);
-push();
-translate(501.5, 390);
-rotate(0.8);
-rect(0, 0, 5, 5);
-pop();
-
-//building 4 (tower)
-fill(204, 158, 89);
-rect(575, 400, 50, 120);
-rect(590, 410, 20, 20);
-push();
-ellipse(600, 360, 80, 30);
-pop();
-push();
-beginShape();
-vertex(570, 389);
-vertex(560, 389);
-vertex(560, 360);
-vertex(640, 360);
-vertex(640, 389);
-endShape();
-pop();
-beginShape();
-vertex(575, 402);
-vertex(561.5, 390);
-vertex(638, 390);
-vertex(625, 402);
-endShape();
-
-//building 3
-ellipse(547, 465, 60, 60);
-rect(503, 470, 90, 60);
-rect(510, 465, 75, 6);
-ellipse(520, 517, 10, 25);
-ellipse(545, 517, 10, 25);
-ellipse(570, 517, 10, 25);
-
-//tower behind building 5
-fill(204, 158, 89);
-rect(741, 455, 20, 40);
-
-//building 5
-rect(693, 425, 3, 30);
-ellipse(695, 497, 100, 90);
-rect(614, 492, 170, 50);
-
-//building 6
-rect(782, 360, 3, 180);
-push();
-translate(783.5, 345);
-rotate(0.8);
-rect(0, 0, 7, 7);
-pop();
+  //ground
+  fill(210,170,109);
+  rect(0, 600, 700);
+  
+  //death star
+  push();
+  fill(102, 102, 102);
+  ellipse(500, 145, 80, 80);
+  pop();
+  push();
+  fill(102, 105, 102);
+  stroke(0,0,0);
+  strokeWeight(3);
+  ellipse(490, 132, 25, 25);
+  rect(460, 155, 80, 0.1);
+  pop();
+  
+  //mos eisley buildings
+  push();
+  translate(0, 80);
+  stroke(166, 120, 51);
+  strokeWeight(3);
+  
+  //building 1
+  fill(204, 158, 89);
+  ellipse(450, 470, 60, 60);
+  rect(400, 470, 50, 70);
+  rect(450, 470, 50, 50);
+  rect(415, 510, 20, 30);
+  push();
+  translate(450, 425);
+  rotate(0.8);
+  rect(0, 0, 10, 10);
+  pop();
+  
+  //building 2 (pole)
+  rect(500, 400, 3, 120);
+  push();
+  translate(501.5, 390);
+  rotate(0.8);
+  rect(0, 0, 5, 5);
+  pop();
+  
+  //building 4 (tower)
+  fill(204, 158, 89);
+  rect(575, 400, 50, 120);
+  rect(590, 410, 20, 20);
+  push();
+  ellipse(600, 360, 80, 30);
+  pop();
+  push();
+  beginShape();
+  vertex(570, 389);
+  vertex(560, 389);
+  vertex(560, 360);
+  vertex(640, 360);
+  vertex(640, 389);
+  endShape();
+  pop();
+  beginShape();
+  vertex(575, 402);
+  vertex(561.5, 390);
+  vertex(638, 390);
+  vertex(625, 402);
+  endShape();
+  
+  //building 3
+  ellipse(547, 465, 60, 60);
+  rect(503, 470, 90, 60);
+  rect(510, 465, 75, 6);
+  ellipse(520, 517, 10, 25);
+  ellipse(545, 517, 10, 25);
+  ellipse(570, 517, 10, 25);
+  
+  //tower behind building 5
+  fill(204, 158, 89);
+  rect(741, 455, 20, 40);
+  
+  //building 5
+  rect(693, 425, 3, 30);
+  ellipse(695, 497, 100, 90);
+  rect(614, 492, 170, 50);
+  
+  //building 6
+  rect(782, 360, 3, 180);
+  push();
+  translate(783.5, 345);
+  rotate(0.8);
+  rect(0, 0, 7, 7);
+  pop();
 }
 
 scenery();
@@ -247,8 +245,20 @@ function restartButton(x, y, w, h) {
     text("Restart game", x + w / 3, y + h / 1.7);
 }
 
+
 function mouseClicked() {
-    console.log("Button was clicked");
+  if (state === "start") {
+    state = "game";
+  } else if (state === "result") {
+    state = "start";
+  }
+}
+
+function buttonClicked() {
+  if (mouseX > 100 && mouseX < 100 + 200 && mouseY > 100 && mouseY < 100 + 60) {
+    console.log("Button is clicked");
+    buttonIsClicked = true;
+  } 
 }
 
 function draw() {
@@ -264,12 +274,4 @@ function draw() {
     } else if (state === "result") {
       resultScreen();
     }
-}
-
-function mouseClicked() {
-  if (state === "start") {
-    state = "game";
-  } else if (state === "result") {
-    state = "start";
-  }
 }
